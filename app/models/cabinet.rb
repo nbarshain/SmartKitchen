40908fe77a7e7db27a23b17518158abfb497e69d
Noah Bar-Shain <nbarshain@gmail.com>
2016-06-21 15:19:30 -0400

    Back to destroy instead of :destroy

--- app/models/cabinet.rb
+++ app/models/cabinet.rb
@@ -1,5 +1,5 @@
 class Cabinet < ActiveRecord::Base
-	has_many :foods, dependent: :destroy
+	has_many :foods, dependent: destroy
 	# has_many :foods
 	belongs_to :kitchen
 	#validates :name, presence: true
