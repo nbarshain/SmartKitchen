e1c1f2ca51b01407d708147e1ebf7effbb816257
Noah Bar-Shain <nbarshain@gmail.com>
2016-06-21 15:06:59 -0400

    Add cabinet belongs to kitchen

--- app/models/cabinet.rb
+++ app/models/cabinet.rb
@@ -1,7 +1,7 @@
 class Cabinet < ActiveRecord::Base
 	#has_many :foods, dependent: destroy
 	has_many :foods
-	belongs_to :kitchen, dependent: destroy
+	belongs_to :kitchen
 	#validates :name, presence: true
 
 end
