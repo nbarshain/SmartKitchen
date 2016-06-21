79e556277a768e8c1fb78fb20cf11226c2d6d2fd
Noah Bar-Shain <nbarshain@gmail.com>
2016-06-21 14:47:25 -0400

    Remove dependency in kitchen model

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -1,4 +1,5 @@
 class Kitchen < ActiveRecord::Base
-	has_many :cabinets, dependent: destroy
-	validates :name, presence: true
+	# has_many :cabinets, dependent: destroy
+	has_many :cabinets
+	# validates :name, presence: true
 end
