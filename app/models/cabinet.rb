daddd204317f6ce08f848b9e7c7b3b3cc21d3b28
Noah Bar-Shain <nbarshain@gmail.com>
2016-06-21 15:04:35 -0400

    Put dependency on belongs to in cabinets

--- app/models/cabinet.rb
+++ app/models/cabinet.rb
@@ -1,7 +1,7 @@
 class Cabinet < ActiveRecord::Base
 	#has_many :foods, dependent: destroy
 	has_many :foods
-	belongs_to :kitchen
+	belongs_to :kitchen, dependent: destroy
 	#validates :name, presence: true
 
 end
