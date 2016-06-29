1754da9f956a8e6ba56784f5a2274ce806e7e9b2
Noah Bar-Shain <nbarshain@gmail.com>
2016-06-29 17:41:15 -0400

    Updating gems

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'cabinets#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+
   post 'foods/:id/bought' => 'foods#bought'
   patch 'foods/:id/bought' => 'foods#update'
 
